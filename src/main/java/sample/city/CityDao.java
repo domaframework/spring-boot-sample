f53fd79e8b639f9ca44c8c1004af08228aeefa24
Toshiaki Maki <makingx@gmail.com>
2015-11-03 20:43:38 +0900

    Apply autoconfigure

--- src/main/java/sample/city/CityDao.java
+++ src/main/java/sample/city/CityDao.java
@@ -5,8 +5,7 @@ import java.util.List;
 import org.seasar.doma.Dao;
 import org.seasar.doma.Select;
 import org.seasar.doma.Update;
-
-import sample.ConfigAutowireable;
+import org.seasar.doma.boot.ConfigAutowireable;
 
 @Dao
 @ConfigAutowireable
